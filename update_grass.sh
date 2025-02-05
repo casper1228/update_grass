e2818a231510fbf5b0d0f69c76af452b0033d2be
casper yang <jby681228@msn.com>
2025-02-05 15:48:58 +0800

    Update update_grass.sh

--- update_grass.sh
+++ update_grass.sh
@@ -3,12 +3,6 @@
 # 定義下載目錄
 DOWNLOAD_DIR="/home/lubuntu/Downloads"
 
-# 函數：關閉 Grass
-close_grass() {
-    echo "正在關閉正在運行的 Grass..."
-    pkill -f grass
-}
-
 # 找到最新的 .deb 文件
 latest_file=$(ls -t "$DOWNLOAD_DIR"/Grass_*.deb 2>/dev/null | head -n 1)
 
@@ -17,9 +11,6 @@ echo "找到的最新文件: $latest_file"
 
 # 檢查最新文件是否存在
 if [ -f "$latest_file" ]; then
-    # 關閉 Grass
-    close_grass
-
     echo "安裝最新更新: $latest_file"
     sudo dpkg -i "$latest_file"  # 安裝 .deb 文件
     sudo apt-get install -f  # 修復依賴
@@ -28,9 +19,6 @@ if [ -f "$latest_file" ]; then
     sudo apt-mark hold grass
     echo "已標記 grass 為不升級。"
 
-    # 重新啟動 Grass
-    echo "正在重新啟動 Grass..."
-    grass &  # 以背景模式啟動 Grass
     echo "更新完成！"
 else
     echo "未找到任何 .deb 文件進行安裝。"
